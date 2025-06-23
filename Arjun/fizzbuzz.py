15060c214333e2a583947d5aeaf002a80617b2b0
arjun selvaraju <106886215+mehavfeeling@users.noreply.github.com>
2025-06-23 16:24:03 -0500

    Update fizzbuzz.py

--- Arjun/fizzbuzz.py
+++ Arjun/fizzbuzz.py
@@ -1,9 +1,9 @@
-#import the mod library
-if number MOD 3 == 0 and number MOD 5 >= 0:
+#import the % library
+if number % 3 == 0 and number % 5 >= 0:
   print("Fizz")
-elif number MOD 5 == 0 and number MOD 3 >= 0:
+elif number % 5 == 0 and number % 3 >= 0:
   print("Buzz")
-elif number MOD 5 == 0 and number MOD 3 == 0:
+elif number % 5 == 0 and number % 3 == 0:
   print("FizzBizz")
 else:
   print(f"Your number, {number}, cannot be divided evenly by the number 3 or the number 5.")
